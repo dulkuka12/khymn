feb11867c675b00c3b0caf82ff2da3b6c5a0834b
dulkuka12 <dulkuka@gmail.com>
2026-09-24 21:56:11 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // 1) 앱 전용 프리픽스
 const CACHE_PREFIX = "khymn-";
 // 배포 시 이 값만 올리세요
-const VERSION = "v2026-09-24-4";
+const VERSION = "v2026-09-24-5";
 const CACHE_NAME = `${CACHE_PREFIX}${VERSION}`;
 
 // 2) 이 앱에 필요한 파일만 절대경로로
